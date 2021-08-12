0a4e34d97dec7c86eb80ca15132a31213f5198b4
Thomas Haller <Thomas.Haller@lab10.coop>
2021-08-12 15:28:12 +0200

    version update for binaries

--- download-binaries.sh
+++ download-binaries.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget -O openethereum https://github.com/DMDcoin/openethereum-3.x/releases/download/v3.2.5-hbbft-alpha-2/openethereum
-wget -O dmd https://github.com/DMDcoin/openethereum-3.x/releases/download/v3.2.5-hbbft-alpha-2/dmd
+wget -O openethereum https://github.com/DMDcoin/openethereum-3.x/releases/download/v3.2.5-hbbft-0.4.0/openethereum
+wget -O dmd https://github.com/DMDcoin/openethereum-3.x/releases/download/v3.2.5-hbbft-0.4.0/dmd
 
 chmod +x openethereum 
 chmod +x dmd
