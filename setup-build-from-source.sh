0a21730cb8f8db731b47cf75b3b85e062e2592d6
Thomas Haller <Thomas.Haller@lab10.coop>
2021-08-30 16:08:30 +0200

    don't build during setup

--- setup-build-from-source.sh
+++ setup-build-from-source.sh
@@ -17,11 +17,3 @@ source $HOME/.cargo/env
 # choose the currect rust version.
 rustup default 1.52.1-x86_64-unknown-linux-gnu
 
-# switch into the directory we have pulled
-cd openethereum-3.x
-
-# build the client.
-cargo build --release
-
-# finally copy the build result to 
-cp openethereum-3.x/target/release/openethereum ./dmdv4-testnet/openethereum
