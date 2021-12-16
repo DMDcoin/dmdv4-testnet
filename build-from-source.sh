1d0395746030a6dd5f168035faa77c990a7fdca7
Thomas Haller <Thomas.Haller@lab10.coop>
2021-12-16 20:23:20 +0100

    - build the client single threated to avoid out of memory problems. - removed `source $HOME/.cargo/env` since it is only required in first install scenarios.

--- build-from-source.sh
+++ build-from-source.sh
@@ -5,13 +5,11 @@ cd openethereum-3.x
 
 git pull
 
-source $HOME/.cargo/env
-
 # choose the currect rust version.
 rustup default 1.52.1-x86_64-unknown-linux-gnu
 
 # build the client.
-cargo build --release --all
+cargo build --release --all -j 1
 
 # finally copy the build result to 
 cp target/release/openethereum ../openethereum
