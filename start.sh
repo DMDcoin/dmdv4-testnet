1ebd7b693bf47043059ebc88c054387bf92c953f
Thomas Haller <Thomas.Haller@lab10.coop>
2022-05-19 23:40:16 +0200

    made start.sh automatic restarting after a 60s delay in preparation for the automatic restart feature in the 0.8.3 update.

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-./openethereum  --no-discovery -c=validator_node.toml
+while [ 1 ]; do
+ ./openethereum -c=validator_node.toml
+ echo "restarting openethereum after 10s sleep..."
+ sleep 60s
+done
