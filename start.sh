86cfb8bf80fc154419fc59bb74bf23dd955b6ea9
Thomas Haller <ThomasHaller@gmx.at>
2022-05-20 19:26:26 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 while [ 1 ]; do
+ echo "starting openethereum..."
  ./openethereum -c=validator_node.toml
- echo "restarting openethereum after 10s sleep..."
+ echo "restarting openethereum after 60s sleep..."
  sleep 60s
 done
