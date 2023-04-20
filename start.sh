558aca1de9987e306ffe3a5b4d63c5690ffa5f5d
Thomas Haller <thomashaller@gmx.at>
2023-04-20 13:19:01 +0000

    start diamond-node instead of openethereum

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 while [ 1 ]; do
  echo "starting openethereum..."
- ./openethereum -c=validator_node.toml
+ ./diamond-node -c=validator_node.toml
  echo "restarting openethereum after 60s sleep..."
  sleep 60s
 done
