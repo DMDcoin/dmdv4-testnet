8193fabec538d9a80df063be0003abeddc870435
Thomas Haller <thomashaller@gmx.at>
2023-06-14 18:22:32 +0000

    build-from-source.base allows to us env var $DMD_NODE_BUILD_FLAGS to define flags. build-from-source.sh behaves the same as before for compatibility.

--- build-from-source.sh
+++ build-from-source.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
 # switch into the directory we have pulled
-cd diamond-node-git
 
-git pull
+export DMD_NODE_BUILD_FLAGS="--all -j 1"
 
-source $HOME/.cargo/env
-
-# choose the currect rust version.
-rustup default 1.68.2-x86_64-unknown-linux-gnu
-
-export RUSTFLAGS='-C target-cpu=native'
-
-# build the client.
-cargo build --release --all -j 1
-
-# finally copy the build result to 
-cp target/release/diamond-node ../diamond-node
-cp target/release/dmd ../dmd
+./build-from-source-base.sh
