6b2e5a8796f0e9805faf12fd969450047a20b6c0
Thomas Haller <thomashaller@gmx.at>
2023-06-15 18:10:24 +0000

    don't do a --all cargo build in every case.

--- build-from-source-base.sh
+++ build-from-source-base.sh
@@ -13,7 +13,7 @@ rustup default 1.68.2-x86_64-unknown-linux-gnu
 export RUSTFLAGS='-C target-cpu=native'
 
 # build the client.
-cargo build --all $DMD_NODE_BUILD_FLAGS
+cargo build $DMD_NODE_BUILD_FLAGS
 
 # finally copy the build result to 
 cp target/release/diamond-node ../diamond-node
