a9228f55dd527c263f359ce3aaed05762b1a9fc1
Thomas Haller <thomashaller@gmx.at>
2023-06-15 15:55:41 +0000

    $DMD_NODE_GIT_TARGET for defining the branch: export DMD_NODE_GIT_TARGET=" --branch main "

---
+++ setup-build-from-source-base.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# this script has only to be run once to clone the repositoriy and install the required tools.
+
+# get the latest source code
+git clone $DMD_NODE_GIT_TARGET https://github.com/DMDcoin/diamond-node.git diamond-node-git
+
+apt-get update
+
+# install required dependencies
+apt-get install build-essential cmake -y
+
+# install rustup so we can build rust software
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# makes rustup availaible
+source $HOME/.cargo/env
+
+# choose the currect rust version.
+rustup default 1.68.2-x86_64-unknown-linux-gnu
+
--- setup-build-from-source.sh
+++ setup-build-from-source.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-# this script has only to be run once to clone the repositoriy and install the required tools.
+export DMD_NODE_GIT_TARGET=" --branch main "
 
-# get the latest source code
-git clone --branch main --single-branch  https://github.com/DMDcoin/diamond-node.git diamond-node-git
-
-
-
-apt-get update
-
-# install required dependencies
-apt-get install build-essential cmake -y
-
-# install rustup so we can build rust software
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-# makes rustup availaible
-source $HOME/.cargo/env
-
-# choose the currect rust version.
-rustup default 1.68.2-x86_64-unknown-linux-gnu
-
+./setup-build-from-source-base.sh
