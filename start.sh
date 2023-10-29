e15ec0d48323e4b336c1bbde9bd2d59a3c569bb9
Thomas Haller <thomashaller@gmx.at>
2023-10-29 10:30:54 +0100

    shutdown_on_missing_block_import has to be defined in config  .toml file ( example: validator_node.toml )

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 while [ 1 ]; do
  echo "starting openethereum..."
- ./diamond-node -c=validator_node.toml --shutdown-on-missing-block-import=1800
+ ./diamond-node -c=validator_node.toml
  echo "restarting openethereum after 60s sleep..."
  sleep 60s
 done
