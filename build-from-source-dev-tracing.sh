d38b8055dbb36a7b7f2c007e1ee29d367c56ac7c
Thomas Haller <thomashaller@gmx.at>
2025-05-27 13:14:47 +0200

    dev-tracing now also includes deadlock detection

--- build-from-source-dev-tracing.sh
+++ build-from-source-dev-tracing.sh
@@ -3,7 +3,7 @@
 # this build script creates a node software with advanced tracing features enabled.
 # the result will have a much lower performance, but is required for developers to get more insights into the node software.
 
-export DMD_NODE_BUILD_FLAGS="--all --profile perf"
+export DMD_NODE_BUILD_FLAGS="--all --profile perf --features deadlock_detection"
 export DMD_NODE_BUILD_PROFILE="perf"
 
 ./_build-from-source-base.sh
