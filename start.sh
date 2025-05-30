033bf0470d316c653090723dc315a1c977419f09
Thomas Haller <thomashaller@gmx.at>
2025-05-30 14:10:44 +0200

    it's better for validators to work with non-persistent message queue, since there are rare cases where transactions are valid at the time when they get constructed, but invalid shortly after, and those transactions need to get removed.

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 while [ 1 ]; do
  echo "starting openethereum..."
- ./diamond-node -c=validator_node.toml
+ ./diamond-node -c=validator_node.toml --no-persistent-txqueue
  echo "restarting openethereum after 60s sleep..."
  sleep 60s
 done
